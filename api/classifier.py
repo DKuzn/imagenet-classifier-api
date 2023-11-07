b9769158af133bc415a0c45d8e0f638d470aa7fd
Dmitry Kuznetsov <DKuznetsov2000@outlook.com>
2023-11-07 18:03:03 +0300

    Add boilerplate code

---
+++ api/classifier.py
@@ -0,0 +1,10 @@
+import onnx
+import onnxruntime as ort
+
+
+class ImageClassifier:
+    def __init__(self) -> None:
+        pass
+
+    def _load_model(path: str):
+        pass
---
+++ api/image_processing.py
@@ -0,0 +1,10 @@
+import opencv
+import numpy as np
+
+class ImageProcessing:
+    def __init__(self) -> None:
+        pass
+
+    @classmethod
+    def decode_image(base64image: str) -> np.ndarray:
+        pass
