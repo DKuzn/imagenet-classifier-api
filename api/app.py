e0d566b84a94c5955d27d74e41b98f907b667b84
Dmitry Kuznetsov <DKuznetsov2000@outlook.com>
2023-11-07 23:21:13 +0300

    Update boilerplate code

--- api/app.py
+++ api/app.py
@@ -1,6 +1,8 @@
 from typing import Union
 
 from fastapi import FastAPI
+from api.datamodels import ClassificationRequest, ClassificationResponse, ImageClass
+from api.image_processing import ImageProcessing
 
 app = FastAPI(title="ImageNet Classifier API")
 
@@ -10,6 +12,14 @@ async def read_root():
     return {"Hello": "World"}
 
 
-@app.get("/items/{item_id}")
-async def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
+@app.post("/classify", response_model=ClassificationResponse)
+async def classify(request: ClassificationRequest):
+    base64_image: str = request.image
+    image = ImageProcessing.decode_base64(base64_image)
+    print(f"Image shape {image.shape}")
+    return ClassificationResponse(
+        classes=[
+            ImageClass(class_id=1, class_name="test1"), 
+            ImageClass(class_id=2, class_name="test2")
+        ]
+    )
--- api/image_processing.py
+++ api/image_processing.py
@@ -1,10 +1,19 @@
-import opencv
+import base64
+
+import cv2 as cv
 import numpy as np
+
 
 class ImageProcessing:
     def __init__(self) -> None:
         pass
 
     @classmethod
-    def decode_image(base64image: str) -> np.ndarray:
-        pass
+    def decode_base64(cls, base64_image: str) -> np.ndarray:
+        np_image: np.ndarray = cls._decode_base64(cls, base64_image)
+        image = cv.imdecode(np_image, cv.IMREAD_COLOR)
+        return cv.cvtColor(image, cv.COLOR_BGR2RGB)
+
+    def _decode_base64(self, base64_image: str) -> np.ndarray:
+        byte_image: bytes = base64.b64decode(base64_image)
+        return np.frombuffer(byte_image, dtype=np.uint8)
