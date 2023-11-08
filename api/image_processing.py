60537ac9024df41cc478484c40128f275672602e
Dmitry Kuznetsov <DKuznetsov2000@outlook.com>
2023-11-08 22:47:39 +0300

    Add resize method

--- api/image_processing.py
+++ api/image_processing.py
@@ -1,4 +1,5 @@
 import base64
+from typing import Tuple
 
 import cv2 as cv
 import numpy as np
@@ -17,3 +18,11 @@ class ImageProcessing:
     def _decode_base64(self, base64_image: str) -> np.ndarray:
         byte_image: bytes = base64.b64decode(base64_image)
         return np.frombuffer(byte_image, dtype=np.uint8)
+    
+    @classmethod
+    def resize(cls, image: np.ndarray, size: Tuple(int, int)) -> np.ndarray:
+        image_shape = image.shape
+        if image_shape[0] > size[0] and image_shape[[1]] > size[1]:
+            return cv.resize(image, size, interpolation=cv.INTER_CUBIC)
+        
+        return cv.resize(image, size, interpolation=cv.INTER_AREA)
