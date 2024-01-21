11a1b3c3588e0dd1eb86639666b8d4ff47dac528
Dmitry Kuznetsov <DKuznetsov2000@outlook.com>
2024-01-21 21:51:36 +0300

    Update tests

--- tests/test_classifier.py
+++ tests/test_classifier.py
@@ -1,8 +1,10 @@
 import os
 import sys
 
-from api.classifier import ImageClassifier
 import pytest
+
+from api.classifier import ImageClassifier
+from api.image_processing import ImageProcessing
 
 current = os.path.dirname(os.path.realpath(__file__))
 parent = os.path.dirname(current)
@@ -19,3 +21,14 @@ def test_load_invalid_model():
 
     with pytest.raises(Exception) as e_info:
         model = ImageClassifier(model_path)
+
+
+def test_prediction():
+    classifier = ImageClassifier("./api/models/mobilenetv3s.onnx")
+    base64_image: str = open("./tests/test_files/test_base64.txt", 'r').read()
+    image = ImageProcessing.decode_base64(base64_image)
+    image = ImageProcessing.to_tensor(image)
+    output = classifier.predict(image)
+    # Получение индекса класса с максимальным значением выхода
+    class_index = output[0][0].argmax(0)
+    assert class_index == 549
--- tests/test_database.py
+++ tests/test_database.py
@@ -11,9 +11,10 @@ current = os.path.dirname(os.path.realpath(__file__))
 parent = os.path.dirname(current)
 sys.path.append(parent)
 
-
-@pytest.mark.asyncio
-async def test_db():
-    data = await DbWrapper.get_users(DbWrapper)
+def test_db():
+    db = DbWrapper()
+    data = db.get_users()
     print(data)
-    assert data == {"name": "name1", "surname": "surname1"}
+
+    assert data.login == "dkuzn1"
+
